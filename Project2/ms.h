b5c9994749798e018c53caadf39240f4712a7262
rla523at <rla523at@naver.com>
2025-05-03 20:55:17 +0900

    정리 merge 전 commit

--- Project2/ms.h
+++ Project2/ms.h
@@ -1,28 +1,97 @@
 #pragma once
-#include "Common.h"
-
-#include <fstream> // std::ifstream
-#include <string>
 #include <algorithm>
+#include <fstream>
+#include <sstream>
+#include <string>
 
 using uint32 = unsigned int;
 
-
-Matrix_Size estimate_size(std::ifstream& file)
+class Table_View
 {
-  constexpr char space = ' ';
+public:
+  Table_View(const uint32 num_rows, const uint32 num_cols, const double* data)
+    : _num_rows(num_rows), _num_cols(num_cols), _data(data) {}
 
-  std::string temp_line;
-  std::getline(file, temp_line); // row 를 알아내야 됨
+  bool operator==(const Table_View& other) const
+  {
+    if ((_num_rows != other._num_rows) || (_num_cols != other._num_cols))
+      return false;
 
-  const uint32 num_columns = static_cast<uint32>(std::count(temp_line.begin(), temp_line.end(), space));
+    const uint32 num_datas = _num_rows * _num_cols;
+    if (std::equal(_data, _data + num_datas, other._data, other._data + num_datas))
+      return true;
+    else
+      return false;
+  }
 
-  uint32 num_rows = 1;
-  while (std::getline(file, temp_line))
-    num_rows++;
-
-  Matrix_Size result;
-  result.num_rows    = num_rows;
-  result.num_columns = num_columns;
-  return result;
+private:
+  uint32        _num_rows;
+  uint32        _num_cols;
+  const double* _data;
 };
+
+class Table
+{
+public:
+  Table(const uint32 num_rows, const uint32 num_cols, std::vector<double>&& datas)
+    : _num_rows(num_rows), _num_cols(num_cols), _datas(datas) {}
+
+public:
+  bool operator==(const Table& other) const
+  {
+    if ((_num_rows != other._num_rows) || (_num_cols != other._num_cols))
+      return false;
+
+    if (std::equal(_datas.begin(), _datas.end(), other._datas.begin(), other._datas.end()))
+      return true;
+    else
+      return false;
+  }
+
+  operator Table_View(void) const
+  {
+    return Table_View(_num_rows, _num_cols, _datas.data());
+  }
+
+  // [start_row, end_row) 
+  Table_View get_subtable_view(const uint32 start_row, const uint32 end_row) const
+  {
+    const uint32  num_rows = end_row - start_row;
+    const double* data     = _datas.data() + start_row * _num_cols;
+
+    Table_View result(num_rows, _num_cols, data);
+    return result;
+  }
+
+private:
+  uint32              _num_rows;
+  uint32              _num_cols;
+  std::vector<double> _datas;
+};
+
+Table make_table(std::ifstream& file)
+{
+  std::istringstream temp_stream;
+  std::string        temp_str;
+
+  double              data = 0.0;
+  std::vector<double> datas;
+
+  uint32 num_rows = 0;
+  while (std::getline(file, temp_str))
+  {
+    temp_stream.clear();
+    temp_stream.str(std::move(temp_str));
+
+    while (temp_stream >> data)
+      datas.push_back(data);
+
+    num_rows++;
+  }
+
+  const uint32 num_data = static_cast<uint32>(datas.size());
+  const uint32 num_cols = num_data / num_rows;
+
+  Table table(num_rows, num_cols, std::move(datas));
+  return table;
+}
---
+++ Sample-Test1/ms_test.cpp
@@ -0,0 +1,115 @@
+#include "pch.h"
+
+#include "../Project2/ms.h"
+
+namespace
+{
+void make_table_file(const char* file_name, const uint32 num_rows, const uint32 num_cols, const std::vector<double>& datas);
+}
+
+TEST(ms, make_table)
+{
+  constexpr const char* file_name = "test.txt";
+  constexpr uint32      num_rows  = 2;
+  constexpr uint32      num_cols  = 3;
+  std::vector<double>   datas     = {1.0, 1.1, 1.3, 2.1, 2.2, 2.3};
+  make_table_file(file_name, num_rows, num_cols, datas);
+
+  std::ifstream read_file(file_name);
+
+  const Table result = make_table(read_file);
+  const Table ref    = {num_rows, num_cols, std::move(datas)};
+
+  EXPECT_EQ(result, ref);
+}
+
+TEST(ms, get_subtable_view)
+{
+  constexpr const char* file_name = "test.txt";
+  constexpr uint32      num_rows  = 4;
+  constexpr uint32      num_cols  = 3;
+  std::vector<double>   datas     = {1.1, 1.2, 1.3, 2.1, 2.2, 2.3, 3.1, 3.2, 3.3, 4.1, 4.2, 4.3};
+  make_table_file(file_name, num_rows, num_cols, datas);
+
+  std::ifstream read_file(file_name);
+
+  const Table      table  = make_table(read_file);
+  const Table_View result = table.get_subtable_view(1, 3); // 2, 3 행을 가져온다;
+
+  constexpr uint32    num_ref_rows = 2;
+  constexpr uint32    num_ref_cols = num_cols;
+  std::vector<double> ref_datas    = {2.1, 2.2, 2.3, 3.1, 3.2, 3.3};
+  const Table_View    ref          = {num_ref_rows, num_ref_cols, ref_datas.data()};
+
+  EXPECT_EQ(result, ref);
+}
+
+namespace
+{
+void make_table_file(const char* file_name, const uint32 num_rows, const uint32 num_cols, const std::vector<double>& datas)
+{
+  std::ofstream out_file(file_name);
+
+  const uint32 last_row = num_rows - 1;
+  const uint32 last_col = num_cols - 1;
+
+  for (uint32 i = 0; i < num_rows; ++i)
+  {
+    for (uint32 j = 0; j < num_cols; ++j)
+    {
+      out_file << datas[i * num_cols + j];
+
+      if (j != last_col)
+        out_file << " ";
+    }
+
+    if (i != last_row)
+      out_file << "\n";
+  }
+}
+} // namespace
+
+//TEST(ms, make_talble_file)
+//{
+//  constexpr const char*     file_name = "test.txt";
+//  constexpr uint32          num_rows  = 2;
+//  constexpr uint32          num_cols  = 3;
+//  const std::vector<double> datas     = {1.01, 1.1, 1.3, 2.1, 2.2, 2.3};
+//  make_table_file(file_name, num_rows, num_cols, datas);
+//
+//  constexpr const char* file_name2 = "ref.txt";
+//  {
+//    std::ofstream out_file(file_name2);
+//    out_file << "1.01 1.1 1.3\n2.1 2.2 2.3";
+//  }
+//
+//  std::ifstream read_file(file_name, std::ios_base::binary);
+//  std::ifstream read_file2(file_name2, std::ios_base::binary);
+//
+//  EXPECT_TRUE(std::equal(
+//    std::istreambuf_iterator<char>(read_file),
+//    std::istreambuf_iterator<char>(),
+//    std::istreambuf_iterator<char>(read_file2)));
+//}
+
+TEST(ms, temp)
+{
+  std::istringstream iss;
+  double             val;
+
+  iss.str("1.0");
+  iss >> val;
+
+  EXPECT_FALSE(iss.fail());
+  EXPECT_TRUE(iss.eof());
+
+  iss.str("2.0");
+  iss >> val;
+
+  // EOF bit 가 켜져있어서 추출 실패
+  EXPECT_TRUE(iss.fail());
+
+  // 추출 실패로 값이 변하지 않는다.
+  EXPECT_NE(val, 2.0);
+  EXPECT_EQ(val, 1.0);
+}
